759a3581b412b874444348202c83eaa5f38d3f42
Ash <github@ash.noblepeople.net>
2025-06-15 16:06:56 +1000

    Added the regular menu

--- script.js
+++ script.js
@@ -1,29 +1,93 @@
 document.addEventListener('DOMContentLoaded', () => {
     const meals = {
+        'Cascade Burger': {
+            type: 'single',
+            variations: [],
+            description: 'House-made tender beef patty, bacon, beetroot, egg, lettuce, tomato, caramelised onion, cheese & tomato chutney, served with a side of beer battered fries'
+        },
         'Chicken Burger': {
             type: 'single',
             variations: [],
-            description: 'Classic chicken burger with lettuce, tomato, and mayo'
+            description: 'Marinated Free Range chicken breast with lettuce, tomato, aioli, bacon, cheese & tomato chutney, served with a side of beer battered fries'
         },
-        'Snitzel': {
+        'Wallaby Burger': {
+            type: 'single',
+            variations: [],
+            description: 'Tender Tassie smoky marinated wallaby fillet, lettuce, tomato, caramelised onion & tomato chutney, served with a side of beer battered fries'
+        },
+        'Moroccan Chickpea Burger': {
+            type: 'single',
+            variations: [],
+            description: 'Grilled chickpea burger with smashed avocado, tomato, lettuce & tomato chutney, served with a side of beer battered fries'
+        },
+        'Fish of the Day': {
+            type: 'single',
+            variations: [],
+            description: 'See our Specials Board for the latest Aquatic Extravaganza or ask any of the Cascade Team'
+        },
+        'Subterranean Pie': {
             type: 'multi',
             variations: [
-                { name: 'Type', options: ['Chicken', 'Beef'] },
                 { name: 'Size', options: ['Small', 'Large'] },
-                { name: 'Sides', options: ['Chips', 'Veggies'] },
-                { name: 'Sauce', options: ['Pepper Sauce', 'Gravy'] }
             ],
-            description: 'Crispy schnitzel with your choice of sides and sauce'
+            description: 'Twice cooked rabbit braised in a rich sauce served with creamy mash & braised black lentils'
         },
-        'Fish and Chips': {
+        'Roast of the Day': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+            ],
+            description: 'See our Specials Board'
+        },
+        'Chicken Parma': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+                { name: 'Sides', options: ['Chips & Salad', 'Veggies'] }
+            ],
+            description: 'Free Range chicken breast served crumbed or pan-fried (gf) with house-made Napoli sauce, ham & mozzarella, served with salad & beer battered fries, or seasonal vegetables'
+        },
+        'Chicken Snitzel': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+                { name: 'Sides', options: ['Chips & Salad', 'Veggies'] },
+                { name: 'Sauce', options: ['Gravy', 'Mushroom', 'Pepper'] }
+            ],
+            description: 'Free Range chicken breast served crumbed or pan-fried (gf) with salad & beer battered fries, or seasonal vegetables & your choice of sauce'
+        },
+        'Siceys Super Beef Snitzel': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+                { name: 'Sides', options: ['Chips & Salad', 'Veggies'] },
+                { name: 'Sauce', options: ['Gravy', 'Mushroom', 'Pepper'] }
+            ],
+            description: 'Free Range chicken breast served crumbed or pan-fried (gf) with salad & beer battered fries, or seasonal vegetables & your choice of sauce'
+        },
+        'Bowl of chips': {
             type: 'single',
             variations: [],
-            description: 'Battered fish with crispy chips'
+            description: 'Bowl of beer battered chips'
         },
-        'Beef Burger': {
+        'Choc fudge Brownie': {
             type: 'single',
             variations: [],
-            description: 'Juicy beef burger with cheese, lettuce, tomato, and mayo'
+            description: 'Can it get any better than this?'
+        },
+        'Fish & Chips': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+            ],
+            description: 'Served with beer battered fries, salad & house-made tartare'
+        },
+        'Rissoles': {
+            type: 'multi',
+            variations: [
+                { name: 'Size', options: ['Small', 'Large'] },
+            ],
+            description: 'Served on creamy mash with a rich gravy'
         }
     };
     const orders = {};
@@ -71,7 +135,6 @@ document.addEventListener('DOMContentLoaded', () => {
             mealCard.innerHTML = `
                 <h3>${mealName}</h3>
                 <p>${meal.description}</p>
-                ${meal.type === 'multi' ? '<p>Customizable options available</p>' : ''}
             `;
             
             mealCard.addEventListener('click', () => {
